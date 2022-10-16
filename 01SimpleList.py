8b96a04b2691f1b30d6c66ea4fbfc6cf80b9a98c
luis <luis.fernandez@iatecc.com>
2022-10-16 10:30:21 +0200

    Simple list example

---
+++ 01SimpleList.py
@@ -0,0 +1,25 @@
+#region
+
+"""
+  This function returns a list of the even values between the specified minimum and maximum
+
+"""
+def getEvenNumbers(min,max):
+  list = []
+  for i in range (min,max):
+    if not i%2:
+      list.append(i)
+  return list
+
+#Keeps a list with the even values between 0 and 10
+myList = getEvenNumbers(0,10)
+
+#prints the entire list
+print(myList)
+
+#Prints some elements
+print(myList[1])
+print(myList[2])
+print(myList[3])
+
+#endregion
